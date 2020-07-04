a55acda3ae49cd0b2cdd7b4771d1b19ef430cd52
Cody P Schafer <dev@codyps.com>
2020-07-04 01:04:23 -0400

    get lazy, use transmute via align_to

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,9 @@
 //!    sending over a network interface
 //! 3. 
 #![warn(missing_docs, rust_2018_idioms, missing_debug_implementations)]
-use std::convert::From;
+use std::mem;
+
+const LOG_MAGIC_V1: u32 = 0x12aab766;
 
 /// The `Log` is an area of memory that is persisted across some (but not necessarily all) events.
 ///
@@ -22,41 +24,79 @@ use std::convert::From;
 /// Log structure itself.
 ///
 /// Physically, the Log structure has the following layout:
-/// ```
-/// struct LogLayout {
-///     /// LOG_MAGIC_V1
-///     magic: u32,
-///     /// Number of bytes considered part of the log
-///     /// This is included so that when the log size changes we can correctly invalidate our
-///     /// header
-///     length: u32,
-///     /// Randomly generated on Log creation. Allows determining if external `Cursors` (which may
-///     /// live past the lifetime of the `Log` by being on remote systems) are still valid.
-///     /// On generation, the lower 32-bits are zeroed. When the log completes a "cycle" the epoch
-///     /// is incremented by 1.
-///     epoch: u64,
 ///
-///     /// Note on `head` and `tail`: the majority of the time, the log is expected to be full.
-///     /// When the log is full these will both be modified by the consumers, and will be directly
-///     /// related to one another. It may be useful to consider another mechanism for encoding
-///     /// this data given the nature of the Log is somewhat different than a normal circular
-///     /// queue.
-///
-///     /// Location where the next entry will be written
-///     head: u16,
-///     /// Location where the last entry is located
-///     tail: u16,
+/// ```norust
+/// struct Log {
+///     header: LogHeader,
+///     data: [data],
 /// }
 /// ```
 #[derive(Debug)]
 pub struct Log<'a> {
+    header: &'a LogHeader,
     data: &'a [u8]
+}
+
+/// LogHeader describes the log so that it can continue to be used across partial-reset events
+///
+/// Note that due to the need to survive partial resets, some care must be had when modifying the
+/// content of the log. The architecture used needs to be able to garuntee that the types of access
+/// used are actually written back to the store that persists across partial-reset events.
+///
+/// Internally, atomics are used to modify fields of the `LogHeader`, but this is only to enable
+/// surviving a partial reset-event, they do _not_ make the `Log` `Sync`: if multiple `Log`
+/// writers (or a writer and any number of readers) exsit, external syncronization mechanisms
+/// (mutexes, interrupt disabling, etc) are required.
+#[derive(Debug)]
+struct LogHeader {
+    /// LOG_MAGIC_V1
+    magic: u32,
+    /// Number of bytes considered part of the log
+    /// This is included so that when the log size changes we can correctly invalidate our
+    /// header
+    length: u32,
+    /// Randomly generated on Log creation. Allows determining if external `Cursors` (which may
+    /// live past the lifetime of the `Log` by being on remote systems) are still valid.
+    /// On generation, the lower 32-bits are zeroed. When the log completes a "cycle" the epoch
+    /// is incremented by 1.
+    epoch: u64,
+
+    /// Note on `head` and `tail`: the majority of the time, the log is expected to be full.
+    /// When the log is full these will both be modified by the consumers, and will be directly
+    /// related to one another. It may be useful to consider another mechanism for encoding
+    /// this data given the nature of the Log is somewhat different than a normal circular
+    /// queue.
+
+    /// Location where the next entry will be written
+    head: u16,
+    /// Location where the last entry is located
+    tail: u16,
 }
 
 impl<'a> From<&'a [u8]> for Log<'a> {
     fn from(data: &'a [u8]) -> Self {
+        let (lh_b, ld) = data.split_at(mem::size_of::<LogHeader>());
+        let lh_m = unsafe {
+            let (h, b, t) = lh_b.align_to::<LogHeader>();
+
+            // If any of these fail, an invalid memory region has been passed to us. Right now, we
+            // consider this a programming error. If a platform exists where the definition of the
+            // memory used for the log is supplied by some configuration, etc. it may make sense to
+            // change this into a `TryFrom` and return a reasonable error.
+            assert_eq!(h.len(), 0);
+            assert_eq!(b.len(), 1);
+            assert_eq!(t.len(), 0);
+
+            &b[0]
+        };
+
+        if lh_m.magic != LOG_MAGIC_V1 {
+            panic!("invalid magic, reformat required");
+        }
+
         Log {
-            data
+            header: lh_m,
+            data: ld,
         }
     }
 }
